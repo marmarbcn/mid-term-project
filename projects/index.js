81cffdc8617aa70364179a8544a67dfd76e59f8a
MartaMartinez <marmarbcn@gmail.com>
2023-03-14 21:10:10 +0100

    JS done

--- projects/index.js
+++ projects/index.js
@@ -1,16 +1,27 @@
-const SERVER_
+const url = 'http://192.168.0.21:3000/projects';
 
-let projects;
-window.addEventListener('load', () => {
-    fetch('http://localhost:3000/projects')
-        .then(response => response.json())
-        .then(data => {
-            projects = data;
-            console.log(projects)
-        })
-        .catch(err => console.error(err));
-    const
+window.onload = () => {
+    const getQueryUuid = () => {
+        const params = new URLSearchParams(window.location.search);
+        return params.get('uuid')
+    }
+
+    const showData = (projects) => {
+        const project = projects.find((project) => project.uuid === getQueryUuid())
+        document.querySelector('.projectTitle').textContent = project.name;
+        document.querySelector('.main-project-img').src = project.image;
+        document.querySelector('.main-project-img-back').src = project.image;
+        document.querySelector('.projectSubtitle').textContent = project.description;
+        document.querySelector('.projectCompleted span').textContent = project.completed_on;
+        document.querySelector('.project').textContent = project.content;
+
+    }
+
+    const getProjectData = () => {
+        fetch(url)
+            .then(response => response.json())
+            .then(response => showData(response))
+    }
+
+    getProjectData();
 }
-)
-
-
